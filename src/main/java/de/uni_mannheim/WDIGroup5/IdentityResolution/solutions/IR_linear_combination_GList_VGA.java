137cce963a28a993050dcf6c51b21127348e2540
yin-feng <g4595616@hotmail.com>
2018-11-19 09:14:40 +0100

    corner cases added

--- src/main/java/de/uni_mannheim/WDIGroup5/IdentityResolution/solutions/IR_linear_combination_GList_VGA.java
+++ src/main/java/de/uni_mannheim/WDIGroup5/IdentityResolution/solutions/IR_linear_combination_GList_VGA.java
@@ -4,9 +4,13 @@ import de.uni_mannheim.WDIGroup5.IdentityResolution.blockers.BlockingByGameTitle
 import de.uni_mannheim.WDIGroup5.IdentityResolution.blockers.BlockingByPlatformGenerator;
 import de.uni_mannheim.WDIGroup5.IdentityResolution.blockers.BlockingByPublisherNameGenerator;
 import de.uni_mannheim.WDIGroup5.IdentityResolution.blockers.BlockingByReleaseYearGenerator;
+import de.uni_mannheim.WDIGroup5.IdentityResolution.comparators.GameGenreComparatorEqual;
+import de.uni_mannheim.WDIGroup5.IdentityResolution.comparators.GamePlatformComparatorEqual;
+import de.uni_mannheim.WDIGroup5.IdentityResolution.comparators.GameReleaseDateComparator2Years;
 import de.uni_mannheim.WDIGroup5.IdentityResolution.comparators.GameTitleComparatorEqual;
 import de.uni_mannheim.WDIGroup5.IdentityResolution.comparators.GameTitleComparatorJaccard;
 import de.uni_mannheim.WDIGroup5.IdentityResolution.comparators.GameTitleComparatorLevenshtein;
+import de.uni_mannheim.WDIGroup5.IdentityResolution.comparators.PublisherNameComparatorEqual;
 import de.uni_mannheim.WDIGroup5.IdentityResolution.model.Game;
 import de.uni_mannheim.WDIGroup5.IdentityResolution.model.GameXMLReader;
 import de.uni_mannheim.informatik.dws.winter.matching.MatchingEngine;
@@ -56,19 +60,22 @@ public class IR_linear_combination_GList_VGA {
         System.out.println("*\n*\tStart Counting Time\n*");
 
         //create a matching rule
-        LinearCombinationMatchingRule<Game, Attribute> matchingRule = new LinearCombinationMatchingRule<>(0.9);
+        LinearCombinationMatchingRule<Game, Attribute> matchingRule = new LinearCombinationMatchingRule<>(0.01);
         //  matchingRule.activateDebugReport("data/output/debugResultsMatchingRule.csv", -1, gsTraining);
 
         //add comparators @Li: Game Title, PublisherName, JapanSales GamePlatform, GameGenre
         //Dont forget to test for equal, levenstein and jaccard
-        matchingRule.addComparator(new GameTitleComparatorJaccard(), 1.0);
+        matchingRule.addComparator(new GamePlatformComparatorEqual(), 0.1);
+        matchingRule.addComparator(new GameTitleComparatorEqual(), 0.9);
+
+
 
         // create a blocker (blocking strategy)
         
-//        System.out.println("*\n*\tStandard Blocker: by title\n*");
-//
-//        StandardRecordBlocker<Game, Attribute> blocker = new StandardRecordBlocker<Game, Attribute>(new BlockingByGameTitleGenerator());
-//        testBlocker(blocker, dataGameList, dataVgaGames, matchingRule, gsTestGamelistVga);
+        System.out.println("*\n*\tStandard Blocker: by title\n*");
+
+        StandardRecordBlocker<Game, Attribute> blocker = new StandardRecordBlocker<Game, Attribute>(new BlockingByGameTitleGenerator());
+        testBlocker(blocker, dataGameList, dataVgaGames, matchingRule, gsTestGamelistVga);
         
         
 //        System.out.println("*\n*\tStandard Blocker: by platform\n*");
