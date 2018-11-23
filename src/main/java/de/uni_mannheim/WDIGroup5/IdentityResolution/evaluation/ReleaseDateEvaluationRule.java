e88726f48ba9305ee38d2d1a1cc554eaed889272
danihoff4 <danihoff@mail.uni-mannheim.de>
2018-11-23 12:42:39 +0100

    pushed release evaluation

--- src/main/java/de/uni_mannheim/WDIGroup5/IdentityResolution/evaluation/ReleaseDateEvaluationRule.java
+++ src/main/java/de/uni_mannheim/WDIGroup5/IdentityResolution/evaluation/ReleaseDateEvaluationRule.java
@@ -10,7 +10,32 @@ import de.uni_mannheim.informatik.dws.winter.similarity.string.TokenizingJaccard
 
 
 
-public class ReleaseDateEvaluationRule {
+public class ReleaseDateEvaluationRule extends EvaluationRule <Game, Attribute>{
 
+	@Override
+	public boolean isEqual(Game record1, Game record2, Attribute schemaElement) {
+
+		if(record1.getReleaseDate()==null && record2.getReleaseDate()==null)
+			return true;
+		else if(record1.getReleaseDate()==null ^ record2.getReleaseDate()==null)
+			return false;
+		else
+			//Returns only true, if Year, Month and Day is equal!
+			return (
+					(record1.getReleaseDate().getYear() == record2.getReleaseDate().getYear()) 
+				   && (record1.getReleaseDate().getMonth() == record2.getReleaseDate().getMonth())
+				   && (record1.getReleaseDate().getDayOfMonth() == record2.getReleaseDate().getDayOfMonth())			
+			);
+			
+		
+	}
+
+	@Override
+	public boolean isEqual(Game record1, Game record2, Correspondence<Attribute, Matchable> schemaCorrespondence) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
 	
 }
