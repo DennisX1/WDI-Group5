78717af7f4f114ab76560b8e2590b1f0c86a8876
danihoff4 <danihoff@mail.uni-mannheim.de>
2018-11-23 16:22:01 +0100

    added publisher fuser!

---
+++ src/main/java/de/uni_mannheim/WDIGroup5/IdentityResolution/fusers/PublisherFuserShortestString.java
@@ -0,0 +1,44 @@
+package de.uni_mannheim.WDIGroup5.IdentityResolution.fusers;
+
+import de.uni_mannheim.WDIGroup5.IdentityResolution.model.Game;
+import de.uni_mannheim.informatik.dws.winter.datafusion.AttributeValueFuser;
+import de.uni_mannheim.informatik.dws.winter.datafusion.conflictresolution.ConflictResolutionFunction;
+import de.uni_mannheim.informatik.dws.winter.datafusion.conflictresolution.string.ShortestString;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.FusedValue;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.RecordGroup;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+
+public class PublisherFuserShortestString extends AttributeValueFuser<String, Game, Attribute> {
+
+	public PublisherFuserShortestString(ConflictResolutionFunction<String, Game, Attribute> conflictResolution) {
+        super(new ShortestString<Game, Attribute>());
+
+	}
+
+	  @Override
+	    public void fuse(RecordGroup<Game, Attribute> group, Game fusedRecord, Processable<Correspondence<Attribute, Matchable>> schemaCorrespondences, Attribute schemaElement) {
+
+	        // get the fused value
+	        FusedValue<String, Game, Attribute> fused = getFusedValue(group, schemaCorrespondences, schemaElement);
+
+	        // set the value for the fused record
+	        fusedRecord.getPublisher().setPublisherName(fused.getValue());
+
+	        // add provenance info
+	        fusedRecord.setAttributeProvenance(Game.PUBLISHER, fused.getOriginalIds());
+	    }
+
+	    @Override
+	    public boolean hasValue(Game record, Correspondence<Attribute, Matchable> correspondence) {
+	        return record.hasValue(Game.PUBLISHER);
+	    }
+
+	    @Override
+	    public String getValue(Game record, Correspondence<Attribute, Matchable> correspondence) {
+	        return record.getPublisher().getPublisherName();
+	    }
+	}
+
